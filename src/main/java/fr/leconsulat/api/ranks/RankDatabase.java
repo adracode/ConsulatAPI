7c39aab10d6e3fa6558548f55c750fe7b98c80c8
Elfas <contact@leconsulat.fr>
2020-01-27 19:03:05 +0100

    add hasAccount/getUUID

--- src/main/java/fr/leconsulat/api/ranks/RankDatabase.java
+++ src/main/java/fr/leconsulat/api/ranks/RankDatabase.java
@@ -23,4 +23,19 @@ public class RankDatabase {
         request.setString(2, player.getName());
         request.executeUpdate();
     }
+
+    public boolean hasAccount(String playerUUID) throws SQLException {
+        PreparedStatement request = this.connection.prepareStatement("SELECT * FROM players WHERE player_uuid = ?");
+        request.setString(1, playerUUID);
+        ResultSet resultSet = request.executeQuery();
+        return resultSet.isBeforeFirst();
+    }
+
+    public String getUUID(String playerName) throws SQLException {
+        PreparedStatement request = this.connection.prepareStatement("SELECT * FROM players WHERE player_name = ?");
+        request.setString(1, playerName);
+        ResultSet resultSet = request.executeQuery();
+        return resultSet.next() ? resultSet.getString("player_uuid") : null;
+    }
+
 }
