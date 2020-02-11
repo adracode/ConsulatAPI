6039fe785434f55c267b05d57863b827dc6e475f
Elfas <contact@leconsulat.fr>
2020-02-11 12:45:31 +0100

    Methodes pour grade perso

--- src/main/java/fr/leconsulat/api/player/ConsulatPlayer.java
+++ src/main/java/fr/leconsulat/api/player/ConsulatPlayer.java
@@ -1,15 +1,20 @@
 package fr.leconsulat.api.player;
 
+import fr.leconsulat.api.custom.CustomObject;
 import fr.leconsulat.api.ranks.RankEnum;
 
 public class ConsulatPlayer {
 
     private RankEnum playerRank;
     private int idPlayer;
-    
-    public ConsulatPlayer(RankEnum playerRank, int id) {
+    private boolean hasPerso;
+    private String persoPrefix;
+
+    public ConsulatPlayer(RankEnum playerRank, int id, CustomObject customObject) {
         this.playerRank = playerRank;
         this.idPlayer = id;
+        this.hasPerso = customObject.isBuyed();
+        this.persoPrefix = customObject.getPrefix();
     }
 
     public RankEnum getRank() {
@@ -22,5 +27,21 @@ public class ConsulatPlayer {
 
     public int getIdPlayer() {
         return idPlayer;
+    }
+
+    public boolean isPerso() {
+        return hasPerso;
+    }
+
+    public String getPersoPrefix() {
+        return persoPrefix;
+    }
+
+    public void setHasPerso(boolean hasPerso) {
+        this.hasPerso = hasPerso;
+    }
+
+    public void setPersoPrefix(String persoPrefix) {
+        this.persoPrefix = persoPrefix;
     }
 }
